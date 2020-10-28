591141d6e3604ed8cf95f1e26ac2884bb12f7733
Michail <mdmurzinov@edu.hse.ru>
2020-10-28 21:44:02 +0300

    Hot Fix

--- home_works/createHelloWorld.sh
+++ home_works/createHelloWorld.sh
@@ -5,15 +5,11 @@ touch cprogramm.c
 echo '#include <stdio.h>
 int main(int argc, char *argv[]){
 FILE *f = fopen("new_programm.sh", "w");
-fprintf(f, "#!/bin/bash\ntouch hello_world.c\necho \"#include <stdio.h>\nint main(int argc, char *argv[]){\nprintf(\\\"Hello World!\\\");\n}\" > hello_world.c\ngcc -o hello_world hello_world.c\n./hello_world\n");
+fprintf(f, "#!/bin/bash\ntouch hello_world.c\necho \"#include <stdio.h>\nint main(int argc, char *argv[]){\nprintf(\\\"Hello World!\\\");\n}\" > hello_world.c\ngcc -o hello_world hello_world.c\n./hello_world\nrm ./hello_world.c ./hello_world");
 popen("bash ./new_programm.sh", "w");
 }' > cprogramm.c
 
 gcc -o cprogramm ./cprogramm.c
 ./cprogramm
 
-rm ./hello_world.c
-rm ./hello_world
-rm ./cprogramm
-rm ./cprogramm.c
-rm ./new_programm.sh
+rm ./cprogramm ./cprogramm.c ./new_programm.sh
